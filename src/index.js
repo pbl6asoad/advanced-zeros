5d29c24caf165c84f8e4c88b9ef4d5af6bc29000
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2019-03-24 18:55:20 +0300

    feat: add counter of zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var countOfZeros = number;
+
+    for (var i = 2; i <= base; i++) {
+        if (base % i === 0) {
+            var tempNumb = number;
+            var numberQual = 0;
+            var countOfNumbInDivide = 0;
+
+            while (base % i === 0) {
+                numberQual++;
+                base = base / i;
+            }
+
+            while (tempNumb > 0) {
+                tempNumb = Math.floor(tempNumb / i);
+                countOfNumbInDivide += tempNumb;
+            }
+
+            countOfZeros = Math.floor(Math.min(countOfZeros, countOfNumbInDivide / numberQual));
+        }
+    }
+    return countOfZeros;
 }
